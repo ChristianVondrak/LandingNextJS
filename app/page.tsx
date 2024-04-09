b1ce0e11122750eba9430f054b469d71ef665320
ChristianVondrak <christian20599@gmail.com>
2024-04-09 16:19:22 -0400

    Avances en el index

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,13 @@ export default function Home() {
     <main className='flex min-h-screen h-fit flex-col items-center justify-center relative'>
       <Navbar />
       <header id="home" className="flex flex-col-reverse md:flex-row w-full h-screen max-w-7xl items-center justify-center p-8 relative ">
-        
+        <div className="w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-black md:text-8xl">Insightful</h1>
+            <h2 className="text-md md:text-2xl">Start growing today</h2>
+          </div>
+
+        </div>
       </header>
     </main>
   )
